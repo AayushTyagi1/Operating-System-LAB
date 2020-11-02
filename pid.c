fb88500dc6fd3a79ba63414434dfc6eee504258b
Aayush Tyagi <tyagiaayush5@gmail.com>
2020-11-02 17:05:15 +0530

    Update pid.c

--- pid.c
+++ pid.c
@@ -1,20 +1,63 @@
-#include<stdio.h>	
-#include<unistd.h>	
-int main()	
-{	
-	int pid = fork();	
-	if(pid<0)	
-	printf("Child process creation unsuccessful!\n");	
-	else if(pid == 0)	
-	{
-		printf("Process id of child: %d\n",getpid());
-		printf("Process id of its parent: %d\n",getppid());
-	}
-	else	
-	{
-		printf("My ID: %d\n",getpid());
-		printf("Process id of parent: %d\n",getppid());
-		printf("Process id of its child: %d\n",pid);
-	}
-	return 0;	
+#include<dirent.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+int main(void)
+{
+    int pid_1,pid_2;
+
+    pid_1 = fork();
+
+    if(pid_1 < 0)
+    {
+         printf("\nError\n");
+    }
+
+    else if(pid_1 == 0)
+    {
+         printf("\nI am first Child");
+         printf("\nMy ID is : %d\n",getpid());
+         printf("My PARENT PROCESS ID is : %d\n",getppid());
+         DIR *d;
+         struct dirent *dir;
+         d = opendir(".");
+         if (d)
+         {
+             while ((dir = readdir(d)) != NULL)
+             {
+                  printf("%s\n", dir->d_name);
+             }
+              closedir(d);
+         }
+    }
+
+    else
+    {
+        sleep(2);
+
+        pid_2 = fork();
+
+        if(pid_2 < 0)
+        {
+            printf("\nError, Second child is not created");
+        }
+
+        else if(pid_2 == 0)
+        {
+            sleep(1);
+            printf("\nI am Second Child");
+            printf("\nMy ID is : %d\n",getpid());
+            printf("\nMy PARENT ID is : %d\n",getppid());
+        }
+
+        else
+        {
+             printf("\nI AM PARENT");
+             printf("\nMy ID is : %d\n",getpid());
+             printf("\nMy CHILD PROCESS ID is : %d\n",pid_2);
+        }
+     }
+
+     return(0);
 }
